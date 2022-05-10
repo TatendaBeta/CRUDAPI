7a81d2103eebf6a248ff54b29ee7afcac3fdc681
Tatenda Beta <tbeta@axissol.com>
2022-05-10 14:56:17 +0200

    Add project files.

---
+++ CRUDAPI/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace CRUDAPI
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string price { get; set; }
+        public string amount { get; set; }
+        public string qty { get; set; }
+    }
+}
---
+++ CRUDAPI/ProductController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDAPI
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        public static List<Product> products = new List<Product>
+            {
+                new Product
+                {
+                    id=1,
+                name = "ruler",
+                price ="1.00",
+                amount="1.00",
+                qty="1.00"
+                }
+            };
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> Get()
+        {
+           
+            return Ok(products);
+        }
+
+        [HttpPost]
+        [Route("AddProduct")]
+        public async Task<ActionResult> AddProduct(Product product)
+        {
+            products.Add(product);
+            return Ok(products);   
+        }
+
+        [HttpPut]
+        [Route ("UpdateProduct")]
+        public async Task<ActionResult<List<Product>>> updateProduct(Product request)
+        {
+            var prod = products.Find(h => h.id == request.id);
+
+            if (prod == null)
+            {
+                return BadRequest("Request no found");
+            }
+
+            prod.name = request.name;
+            return Ok(products);
+        }
+
+
+    }
+
+
+}
